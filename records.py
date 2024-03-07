f187e4bda00f386216f2dd4bb732930c78d7fedb
camfung <cameronfuung@gmail.com>
2024-03-07 01:28:27 -0800

    updated the seconds to show 2 decimal points

--- records.py
+++ records.py
@@ -37,7 +37,7 @@ class Record:
         :return: String including index, player turn, move, move type and timestamp
         """
         # return f"{self._index}: {self._player_turn} {self._move} - {int(self._time_taken)} seconds"
-        return f"{self._index}: {self._player_turn.name} {self._move} {self._move.get_move_type().name}  {int(self._time_taken)} seconds"
+        return f"{self._index}: {self._player_turn.name} {self._move} {self._move.get_move_type().name}  {format(self._time_taken, '.2f')} seconds"
 
 
 class RecordHistory:
