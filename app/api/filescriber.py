35340685188819e0262f488238cfdb72585172f1
Joseph Driedger <jdriedger3@my.bcit.ca>
2024-03-12 23:21:08 -0700

    Created a new Class called FileScriber. Used for Chi En's Part 2 Testing our State Space Generation.

---
+++ app/api/filescriber.py
@@ -0,0 +1,29 @@
+
+from app.gameplay.game_state import GameState
+
+
+class FileScriber:
+    @staticmethod
+    def export_state_space_to_text_files(input_file, output_move_file, output_board_file):
+        # Step 1: Read Input File
+
+        # Step 2: Read Current Player's Turn
+
+        # Step 3: Read Board Configuration
+
+        # Step 4: Create New GameState from the Board Configuration (Step 3) & Current Player's Turn (Step 2)
+
+        # Step 5: Get GameState's State Space Generation (List of Moves & Boards)
+
+        # Step 6: Export List of Moves to Output Move File
+
+        # Step 7: Export List of Boards to Output Board File
+        pass
+
+    @staticmethod
+    def import_board_from_text_files(input_file):
+        # Read Board File
+
+        # Return Results
+        pass
+
---
+++ test/player/state_space_test/StateSpaceTest.py
@@ -0,0 +1,29 @@
+
+
+import unittest
+from app.api.filescriber import FileScriber
+
+
+class StateSpaceTest(unittest.TestCase):
+    def chi_en_test_1(self):
+        FileScriber.export_state_space_to_text_files("./Input/Test1.input",
+                                                     "./Output/Test1.move",
+                                                     "./Output/Test1.board")
+        exported_results = FileScriber.import_board_from_text_files("./Output/Test1.board")
+        verified_results = FileScriber.import_board_from_text_files("./Verify/Test1.board")
+        self.assertEqual(exported_results, verified_results)
+
+    def chi_en_test_2(self):
+        FileScriber.export_state_space_to_text_files("./Input/Test2.input",
+                                                     "./Output/Test2.move",
+                                                     "./Output/Test2.board")
+        exported_results = FileScriber.import_board_from_text_files("./Output/Test2.board")
+        verified_results = FileScriber.import_board_from_text_files("./Verify/Test2.board")
+        self.assertEqual(exported_results, verified_results)
+
+    def team_num_balls_test_1(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
