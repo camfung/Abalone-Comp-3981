9ed705cb3fb7dc17746f2ac3d2ffdd5b2c081259
Callum Goss <callumgoss@protonmail.com>
2024-03-13 15:35:54 -0700

    Changed move limit input to text box, also made limits on the how large the number can be on the inputs

--- app/ui/ui.py
+++ app/ui/ui.py
@@ -198,12 +198,11 @@ class PygameUI(UI):
             (f.name, f) for f in Formation], default=0, onchange=self.update_play_button)
 
         # Adding time limit and move limit selectors for both Black and White players
-        black_time_limit = menu.add.text_input('Black Time Limit (Seconds): ', default = 30, input_type=pygame_menu.locals.INPUT_INT)
+        black_time_limit = menu.add.text_input('Black Time Limit (Seconds): ', default = 30, input_type=pygame_menu.locals.INPUT_INT, maxchar=4)
 
-        white_time_limit = menu.add.text_input('White Time Limit (Seconds): ', default = 30, input_type=pygame_menu.locals.INPUT_INT)
+        white_time_limit = menu.add.text_input('White Time Limit (Seconds): ', default = 30, input_type=pygame_menu.locals.INPUT_INT, maxchar=4)
 
-        move_limit = menu.add.dropselect('Move Limit: ', [
-            (f'{x} moves', x) for x in range(5, 100, 5)], default=3)
+        move_limit = menu.add.text_input('Move Limit: ', default = 20, input_type=pygame_menu.locals.INPUT_INT, maxchar = 3)
 
         menu.add.button('Play', lambda: self.start_the_game({
             'game_type': opponent_type.get_value(),
