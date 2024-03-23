234e403514ddbae2aa309c704bc7c1c9a96d8e35
Joseph Driedger <jdriedger3@my.bcit.ca>
2024-03-23 15:21:22 -0700

    Completed These Functions

--- app/players/test_agents/joey.py
+++ app/players/test_agents/joey.py
@@ -20,16 +20,23 @@ class AgentJoey(AbaloneAgent):
         total_reward += pieces[1]
 
         total_reward += cls.grouped_together(board, black_positions, white_positions)
-        total_reward += cls.center_of_board(board, black_positions, white_positions)
+        total_reward += cls.center_of_board(black_positions, white_positions)
         total_reward += cls.anchored_pieces(board, black_positions, white_positions)
 
         return total_reward
 
     @classmethod
     def find_positions_of_pieces(cls, board):
+        """
+        Find positions of all pieces on board.
+        Possibly implemented into GameState instead of Agent to be stored in GameState.
+        :param board: Array Representation of GameState Board
+        :return: Tuple containing List of Black Positions and White Positions
+        """
         black_positions = []
         white_positions = []
 
+        # Loop through every spot on the board.
         for rIndex, row in enumerate(board):
             for cIndex, col in enumerate(row):
                 if col == Marble.BLACK:
@@ -41,8 +48,16 @@ class AgentJoey(AbaloneAgent):
 
     @classmethod
     def grouped_together(cls, board, black_positions, white_positions):
+        """
+        Reward Sides for Grouping Together.
+        :param board: Array Representation of GameState Board
+        :param black_positions: List of Tuples representing Black Positions
+        :param white_positions: List of Tuples representing White Positions
+        :return: int value indicating total reward
+        """
         total_reward = 0
 
+        # Calculate Black's Reward
         for black_position in black_positions:
             for direction in Direction:
                 pos_x = black_position[0]
@@ -50,9 +65,11 @@ class AgentJoey(AbaloneAgent):
                 dir_x = direction.value[0]
                 dir_y = direction.value[1]
 
-                if board[pos_x + dir_x][pos_y + dir_y] == Marble.BLACK:
-                    total_reward += 1
+                for i in range(1, 3):
+                    if board[pos_x + (dir_x * i)][pos_y + (dir_y * i)] == Marble.BLACK:
+                        total_reward += i
 
+        # Calculate White's Reward
         for white_position in white_positions:
             for direction in Direction:
                 pos_x = white_position[0]
@@ -60,13 +77,21 @@ class AgentJoey(AbaloneAgent):
                 dir_x = direction.value[0]
                 dir_y = direction.value[1]
 
-                if board[pos_x + dir_x][pos_y + dir_y] == Marble.WHITE:
-                    total_reward -= 1
+                for i in range(1, 3):
+                    if board[pos_x + (dir_x * i)][pos_y + (dir_y * i)] == Marble.WHITE:
+                        total_reward -= i
 
         return total_reward
 
     @classmethod
-    def center_of_board(cls, board, black_positions, white_positions):
+    def center_of_board(cls, black_positions, white_positions):
+        """
+        Reward Players for having pieces at Center of Board
+
+        :param black_positions: List of Tuples representing Black Positions
+        :param white_positions: List of Tuples representing White Positions
+        :return: int value indicating total reward
+        """
         total_reward = 0
         center_board = (5, 5)
 
@@ -96,11 +121,22 @@ class AgentJoey(AbaloneAgent):
 
     @classmethod
     def anchored_pieces(cls, board, black_positions, white_positions):
+        """
+        Punish Players for having single pieces close to being surrounded or is completely by opposing pieces
+
+        :param board: Array Representation of GameState Board
+        :param black_positions: List of Tuples representing Black Positions
+        :param white_positions: List of Tuples representing White Positions
+        :return: int value indicating total reward
+        """
         total_reward = 0
 
+        # Calculate Black's Punishment
         for black_position in black_positions:
             pos_x = black_position[0]
             pos_y = black_position[1]
+
+            pieces_surrounded = 0
 
             # Check for All 6 Directions
             for direction in Direction:
@@ -108,17 +144,27 @@ class AgentJoey(AbaloneAgent):
                 dir_y = direction.value[1]
 
                 if board[pos_x + dir_x][pos_y + dir_y] == Marble.WHITE:
-                    total_reward -= 1
+                    pieces_surrounded += 1
 
+            if pieces_surrounded > 4:
+                total_reward -= (pieces_surrounded * 10)
+
+        # Calculate White's Punishment
         for white_position in white_positions:
             pos_x = white_position[0]
             pos_y = white_position[1]
 
+            pieces_surrounded = 0
+
+            # Check for All 6 Directions
             for direction in Direction:
                 dir_x = direction.value[0]
                 dir_y = direction.value[1]
 
                 if board[pos_x + dir_x][pos_y + dir_y] == Marble.BLACK:
-                    total_reward -= 1
+                    pieces_surrounded += 1
+
+            if pieces_surrounded > 4:
+                total_reward += (pieces_surrounded * 10)
 
         return total_reward
