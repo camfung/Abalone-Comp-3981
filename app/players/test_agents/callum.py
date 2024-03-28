00e8399064eee3968e4c425662872c62572286a5
Callum Goss <callumgoss@protonmail.com>
2024-03-28 14:35:01 -0700

    Added score evaluation to Callum Agent

--- app/players/test_agents/callum.py
+++ app/players/test_agents/callum.py
@@ -8,8 +8,7 @@ To Be Implemented in Part 3.
 
 class AgentCallum(AbaloneAgent):
 
-    @classmethod
-    def evaluation(cls, state):
+    def evaluation(self, state):
         """
         Evaluate the current state based on heuristics.
 
@@ -25,43 +24,51 @@ class AgentCallum(AbaloneAgent):
         ##check if time is up, if it is return best move so far
 
         board = state._board
-        bunch = evaluate_bunching(board)
-        distance_edge = distance_from_edge(board)
+        bunch = self.evaluate_bunching(board)
+        distance_edge = self.distance_from_edge(board)
         white_balls, black_balls = state.get_ball_count()
 
-        return bunch + distance_edge
+        if self.color == Marble.WHITE:
+            self.balls = white_balls
+            self.opponent_balls = black_balls
+        else:
+            self.balls = black_balls
+            self.opponent_balls = white_balls
+
+        return bunch + distance_edge + 90 * (self.balls - self.opponent_balls)
 
 
-def evaluate_bunching(board):
-    bunching_scores = 0
+    def evaluate_bunching(self, board):
+        bunching_scores = 0
 
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if board[i][j] == Marble.WHITE:
-                bunching_scores += count_neighbors(board, i, j)
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] == self.color:
+                    bunching_scores += self.count_neighbors(board, i, j)
 
-    return bunching_scores
+        return bunching_scores
 
-def count_neighbors(board, x, y):
-    neighbors = 0
-    for dx in range(-1, 2):
-        for dy in range(-1, 2):
-            if dx == 0 and dy == 0:
-                continue  # Skip the current position
-            new_x = x + dx
-            new_y = y + dy
-            if 0 <= new_x < len(board) and 0 <= new_y < len(board[0]) and board[new_x][new_y] == Marble.WHITE:
-                neighbors += 1
-    return neighbors
+    def count_neighbors(self, board, x, y):
+        neighbors = 0
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                if dx == 0 and dy == 0:
+                    continue  # Skip the current position
+                new_x = x + dx
+                new_y = y + dy
+                if 0 <= new_x < len(board) and 0 <= new_y < len(board[0]) and board[new_x][new_y] == Marble.WHITE:
+                    neighbors += 1
+        return neighbors
 
 
-def distance_from_edge(board):
-    distances = 0
+    def distance_from_edge(self, board):
+        distances = 0
 
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if board[i][j] == Marble.WHITE:
-                min_distance = min(i, j, len(board) - 1 - i, len(board[0]) - 1 - j)
-                distances += min_distance
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] == self.color:
+                    distance = abs(5 - i) + abs(5 - j)
+                    distance =  max(0, 11 - distance)
+                    distances += distance
 
-    return distances
+        return distances
